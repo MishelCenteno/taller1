e9b6a29c0af2774e84ae38976c29696583a79449
USUARIO04-PC\Usuario01 <mishelcenteno96@gmail.com>
2016-12-14 12:14:07 -0500

    Ejercicio 4-5

--- Ejercicios/Ejercicio 1-2-3.py
+++ Ejercicios/Ejercicio 1-2-3.py
@@ -33,7 +33,56 @@ def ejercicio3():
     root = Tk()
     app = App(root)
     root.mainloop()
+def ejercicio4():
+    root = Tk()
+    v = IntVar()
+    Label(root,
+     text="""Elija el lenguaje de programacion:""",
+     justify = LEFT,
+     padx = 20).pack()
+    Radiobutton(root,
+     text="Python",
+     padx = 20,
+     variable=v,
+     value=1).pack(anchor=W)
+    Radiobutton(root,
+     text="Perl",
+     padx = 20,
+     variable=v,
+     value=2).pack(anchor=W)
+
+    mainloop()
+    
+    
+def ejercicio5():
+    root = Tk()
+    v = IntVar()
+    v.set(1) # initializing the choice, i.e. Python
+    languages = [
+     ("Python",1),
+     ("Perl",2),
+     ("Java",3),
+     ("C++",4),
+     ("C",5)
+    ]
+    def MostrarOpcion():
+     print (v.get())
+    Label(root,
+     text="""Elije tu lenguaje favorito para programar:""",
+     justify = LEFT,
+     padx = 20).pack()
+    for txt, val in languages:
+     Radiobutton(root,
+     text=txt,
+     padx = 30,
+    variable=v,
+     command=MostrarOpcion,
+     value=val).pack(anchor=W)
+    mainloop()
+
 
 ejercicio1()
 ejercicio2()
 ejercicio3()
+ejercicio4()
+ejercicio5()
